62c8a5acfa41f3f30de7b5de15ca07d5fc929142
flabiohs <flabiohs@gmail.com>
2025-12-08 19:32:42 -0500

    fix:release_2026

---
+++ springboot-id-factura/src/main/java/com/flabiohs/springboot/di/factura/springboot_id_factura/SpringbootIdFacturaApplication.java
@@ -0,0 +1,13 @@
+package com.flabiohs.springboot.di.factura.springboot_id_factura;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootIdFacturaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootIdFacturaApplication.class, args);
+	}
+
+}
---
+++ springboot-id-factura/src/test/java/com/flabiohs/springboot/di/factura/springboot_id_factura/SpringbootIdFacturaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flabiohs.springboot.di.factura.springboot_id_factura;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootIdFacturaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
